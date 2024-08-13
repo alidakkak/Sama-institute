083a82adeb3f5a0753de1c7dd08d64b6ac003d59
Ali Dakkak <alidakak21@gmail.com>
2024-08-13 04:47:26 +0300

    zk

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -1,62 +1,62 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Models\ImportLog;
-use App\Models\InOutLog;
-use App\Models\Student;
-use Jmrashed\Zkteco\Lib\ZKTeco;
-
-class AttendanceController extends Controller
-{
-    public function fetchAttendance()
-    {
-        $zk = new ZKTeco('192.168.1.201');
-
-        if ($zk->connect()) {
-            $attendances = $zk->getAttendance();
-
-            // الحصول على آخر وقت استيراد من قاعدة البيانات
-            $lastImport = ImportLog::orderBy('last_import_time', 'desc')->first();
-            $lastImportTime = $lastImport ? $lastImport->last_import_time : null;
-
-            foreach ($attendances as $attendance) {
-                $attendanceTime = date('Y-m-d H:i:s', strtotime($attendance['timestamp']));
-
-                // استيراد فقط السجلات التي تمت بعد آخر وقت استيراد
-                if ($lastImportTime === null || $attendanceTime > $lastImportTime) {
-                    $student = Student::where('device_user_id', $attendance['uid'])->first();
-
-                    if ($student) {
-                        $log = InOutLog::where('student_id', $student->id)
-                            ->whereDate('in_time', date('Y-m-d', strtotime($attendance['timestamp'])))
-                            ->first();
-
-                        if ($log) {
-                            $log->out_time = $attendanceTime;
-                        } else {
-                            $log = new InOutLog();
-                            $log->student_id = $student->id;
-                            $log->in_time = $attendanceTime;
-                        }
-
-                        $log->save();
-                    }
-                }
-            }
-            ImportLog::create(['last_import_time' => now()]);
-
-            $zk->disconnect();
-        }
-    }
-
-    public function test()
-    {
-        $zk = new ZKTeco('192.168.1.201');
-        if ($zk->connect()) {
-            return response('OK', 200);
-        }else
-            return response('Connection error', 500);
-    }
-
-}
+//
+//namespace App\Http\Controllers;
+//
+//use App\Models\ImportLog;
+//use App\Models\InOutLog;
+//use App\Models\Student;
+//use Jmrashed\Zkteco\Lib\ZKTeco;
+//
+//class AttendanceController extends Controller
+//{
+//    public function fetchAttendance()
+//    {
+//        $zk = new ZKTeco('192.168.1.201');
+//
+//        if ($zk->connect()) {
+//            $attendances = $zk->getAttendance();
+//
+//            // الحصول على آخر وقت استيراد من قاعدة البيانات
+//            $lastImport = ImportLog::orderBy('last_import_time', 'desc')->first();
+//            $lastImportTime = $lastImport ? $lastImport->last_import_time : null;
+//
+//            foreach ($attendances as $attendance) {
+//                $attendanceTime = date('Y-m-d H:i:s', strtotime($attendance['timestamp']));
+//
+//                // استيراد فقط السجلات التي تمت بعد آخر وقت استيراد
+//                if ($lastImportTime === null || $attendanceTime > $lastImportTime) {
+//                    $student = Student::where('device_user_id', $attendance['uid'])->first();
+//
+//                    if ($student) {
+//                        $log = InOutLog::where('student_id', $student->id)
+//                            ->whereDate('in_time', date('Y-m-d', strtotime($attendance['timestamp'])))
+//                            ->first();
+//
+//                        if ($log) {
+//                            $log->out_time = $attendanceTime;
+//                        } else {
+//                            $log = new InOutLog();
+//                            $log->student_id = $student->id;
+//                            $log->in_time = $attendanceTime;
+//                        }
+//
+//                        $log->save();
+//                    }
+//                }
+//            }
+//            ImportLog::create(['last_import_time' => now()]);
+//
+//            $zk->disconnect();
+//        }
+//    }
+//
+//    public function test()
+//    {
+//        $zk = new ZKTeco('192.168.1.201');
+//        if ($zk->connect()) {
+//            return response('OK', 200);
+//        }else
+//            return response('Connection error', 500);
+//    }
+//
+//}
