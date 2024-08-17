7dc6f5a57f9809fbfa9993473e830b4039ba1bac
Ali Dakkak <alidakak21@gmail.com>
2024-08-18 00:55:49 +0300

    sa

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExtraCharge;
 use App\Models\GeneralExpense;
 use App\Models\Registration;
 use App\Models\Semester;
 use App\Models\Student;
+use App\Models\StudentPayment;
 use App\Models\Teacher;
 use App\Models\TeacherSalary;
 
@@ -59,8 +61,32 @@ class StatisticController extends Controller
 
     public function financialResults()
     {
-        $teacher = TeacherSalary::all()->sum('price');
-        $generalExpense = GeneralExpense::all()->sum('price');
-        return $generalExpense;
+        // إجمالي الرواتب المدفوعة للمعلمين
+        $totalTeacherSalary = TeacherSalary::all()->sum('price');
+
+        // إجمالي المصروفات العامة
+        $totalGeneralExpense = GeneralExpense::all()->sum('price');
+
+        // إجمالي المدفوعات المستلمة من الطلاب
+        $totalStudentPayments = StudentPayment::all()->sum('price');
+
+
+        $extraCharge = ExtraCharge::all()->sum('price');
+
+        // إجمالي النفقات (الرواتب والمصروفات العامة)
+        $totalExpenses = $totalTeacherSalary + $totalGeneralExpense;
+
+        // صافي الأرباح أو الخسائر
+        $netProfitOrLoss = ($totalStudentPayments + $extraCharge) - $totalExpenses;
+
+        return response()->json([
+            'totalTeacherSalary' => $totalTeacherSalary,
+            'totalGeneralExpense' => $totalGeneralExpense,
+            'totalStudentPayments' => $totalStudentPayments,
+            'extraCharge' => $extraCharge,
+            'totalExpenses' => $totalExpenses,
+            'netProfitOrLoss' => $netProfitOrLoss,
+        ]);
     }
+
 }
